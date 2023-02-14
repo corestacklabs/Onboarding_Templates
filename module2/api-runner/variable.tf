fd3a835fbfb8402652fc6ec12eabf05bd28af96e
Rishi Agrawal <rishi.agrawal@corestack.io>
2023-02-14 21:18:40 +0530

    Bug fixes and new permissions

--- module2/api-runner/variable.tf
+++ module2/api-runner/variable.tf
@@ -2,3 +2,7 @@ variable "api" {
 type = set(string)
 description = "List of APIS that needs to be enabled per project"
 }
+variable "orgproject" {
+  type = string
+  description = "org proj"
+}
--- module2/extractor.py
+++ module2/extractor.py
@@ -1,5 +1,5 @@
 import os
-os.system(" terraform output > li_p.txt")
+os.system("terraform output > li_p.txt")
 f = open("li_p.txt", "r")
 li = []
 for x in f:
@@ -24,9 +24,7 @@ vaf.close()
 os.system("terraform -chdir=./api-runner/ init")
 for i in li_:
     vf = open("api-runner/api.tfvars","w")
-    vf.write(f'''
-    {x}
-    orgproj = "{i}"
-    ''')
+    vf.write(f'''orgproject = "{i}"
+    {x}''')
     vf.close()
     os.system(f'terraform -chdir=./api-runner/ apply -auto-approve -var-file="api.tfvars"')
