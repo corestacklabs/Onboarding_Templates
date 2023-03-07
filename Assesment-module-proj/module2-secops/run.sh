e35ba676af9280437d7fcd51f068f5aee600ab5b
Rishi Agrawal <rishi.agrawal@corestack.io>
2023-03-07 17:10:27 +0530

    Added run.sh

---
+++ Assesment-module-proj/module2-secops/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Please ensure you have the necessary IAM permissions .The execution of this script requires following user account permissions: "
+echo "Project Owner"
+read -p "Please specify yes if above pre-requisites met or specify no : " INPUT 
+
+if [ "${INPUT}" == "No" ] || [ "${INPUT}" == "no" ] ; then
+     # exiting with return code 0
+   echo 'Exiting..'
+   exit 0
+
+elif [ "${INPUT}" == "Yes" ]||[ "${INPUT}" == "yes" ] ; then   
+terraform init
+terraform apply -var-file="vars.tfvars" -auto-approve
+echo "Approved"
+fi
