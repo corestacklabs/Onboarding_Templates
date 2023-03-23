cf6d43b8e27ab2e4234200f0daba2818992ce909
Rishi Agrawal <rishi.agrawal@corestack.io>
2023-03-23 15:43:29 +0530

    fix

--- Assesment-module-proj/core/run.sh
+++ Assesment-module-proj/core/run.sh
@@ -6,40 +6,36 @@ if [ -f vars.tfvars ]; then
 else
   read -p "Please specify yes if above pre-requisites met or specify no : " INPUT
   if [ "${INPUT}" == "No" ] || [ "${INPUT}" == "no" ] ; then
-     # exiting with return code 0
     echo 'Exiting..'
     exit 0 
   elif [ "${INPUT}" == "Yes" ]||[ "${INPUT}" == "yes" ] ; then   
-  read -p "Please enter the project id: " proj_id
-  read -p "Please enter the service account email: " svcacc
-  read -p "Assign predefined role?(roles/viewer) specify yes or no: " role
-  if [ "${role}" == "No" ] || [ "${role}" == "no" ] ; then
-  read -p "Do you need to retrive vunrelabilities and threat from GCP console? specify yes or no:" secops
-  read -p "Creating custom role, provide the name: " roleid
-  if [ "${secops}" == "No" ] || [ "${secops}" == "no" ] ; then
-  cat <<EOF > ./vars.tfvars
+    read -p "Please enter the project id: " proj_id
+    read -p "Please enter the service account email: " svcacc
+    read -p "Assign predefined role?(roles/viewer) specify yes or no: " role
+    if [ "${role}" == "No" ] || [ "${role}" == "no" ] ; then
+      read -p "Do you need to retrive vunrelabilities and threat from GCP console? specify yes or no:" secops
+      read -p "Creating custom role, provide the name: " roleid
+      if [ "${secops}" == "No" ] || [ "${secops}" == "no" ] ; then
+        cat <<EOF > ./vars.tfvars
 project_id = "$proj_id"
 service_account_email = "$svcacc"
 role_id = "$roleid"
 EOF
-  elif [ "${secops}" == "Yes" ] || [ "${secops}" == "yes" ] ; then
-    cat <<EOF > ./vars.tfvars
+      elif [ "${secops}" == "Yes" ] || [ "${secops}" == "yes" ] ; then
+        cat <<EOF > ./vars.tfvars
 project_id = "$proj_id"
 service_account_email = "$svcacc"
 role_id = "$roleid"
 permissionsec = "yes"
 EOF
-  fi
-
-  elif [ "${role}" == "Yes" ]||[ "${role}" == "yes" ] ; then
-  echo "granting predefined roles:(roles/viewer)"
-  cat <<EOF > ./vars.tfvars
+      elif [ "${role}" == "Yes" ]||[ "${role}" == "yes" ] ; then
+        echo "granting predefined roles:(roles/viewer)"
+        cat <<EOF > ./vars.tfvars
 project_id = "$proj_id"
 service_account_email = "$svcacc"
 EOF
-  fi
 fi
 terraform init
 terraform apply -var-file="vars.tfvars" -auto-approve
 echo "Approved"
-fi
+EOF
