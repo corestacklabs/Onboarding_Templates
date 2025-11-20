52326240c773d02623d47c628fccfa4365242efd
Rishi Agrawal <rishi.agrawal@corestack.io>
2025-11-20 16:45:06 +0530

    Refactor BigQuery transfer configs for Parquet format
    Updated BigQuery data transfer configurations to use Parquet format with Snappy compression and modified query parameters.

--- GCP/Billing-account/main.tf
+++ GCP/Billing-account/main.tf
@@ -37,67 +37,135 @@ resource "google_storage_bucket" "bucket" {
 }
 
 # Resource for Query1
-resource "google_bigquery_data_transfer_config" "query_config_1"{
-      display_name           = "corestack-daily-query-${var.project_id}"
-      project                = var.project_id
-      location               = "${var.bucket_location}"
-      data_source_id         = "scheduled_query"
-      schedule               = "every 24 hours"
-      params = {
-        query = "DECLARE bucket_name STRING DEFAULT '${local.bucket_name}'; DECLARE current_date DATE DEFAULT CURRENT_DATE; DECLARE start_date TIMESTAMP DEFAULT TIMESTAMP(DATE_TRUNC(current_date, MONTH), 'US/Pacific'); DECLARE end_date TIMESTAMP DEFAULT TIMESTAMP(DATE_TRUNC(DATE_ADD(current_date, INTERVAL 1 MONTH), MONTH), 'US/Pacific'); DECLARE bucket_path STRING DEFAULT CONCAT('gs://', bucket_name, '/', FORMAT_DATE('%G-%m', current_date), '/', FORMAT_DATE('%F', current_date), '/*.csv'); EXPORT DATA OPTIONS (uri=(bucket_path), format='JSON', overwrite=True) AS SELECT *, (SELECT STRING_AGG(display_name, '/') FROM B.project.ancestors) organization_list FROM `${var.table_id}` as B WHERE usage_start_time >= start_date AND usage_start_time < end_date AND _PARTITIONTIME >= TIMESTAMP(EXTRACT(DATE FROM start_date)) ;"
-      }
-    }
+resource "google_bigquery_data_transfer_config" "query_config_1" {
+  display_name   = "corestack-daily-query-${var.project_id}"
+  project        = var.project_id
+  location       = "${var.bucket_location}"
+  data_source_id = "scheduled_query"
+  schedule       = "every 24 hours"
+
+  params = {
+    query = <<-EOF
+      DECLARE bucket_name STRING DEFAULT '${local.bucket_name}';  
+      DECLARE current_date DATE DEFAULT CURRENT_DATE;
+      -- Daily  
+      DECLARE start_date TIMESTAMP DEFAULT TIMESTAMP(DATE_TRUNC(current_date, MONTH), 'US/Pacific');  
+      DECLARE end_date TIMESTAMP DEFAULT TIMESTAMP(DATE_TRUNC(DATE_ADD(current_date, INTERVAL 1 MONTH), MONTH), 'US/Pacific');  
+      DECLARE bucket_path STRING DEFAULT CONCAT('gs://', bucket_name, '/', FORMAT_DATE('%G-%m', current_date), '/', FORMAT_DATE('%F', current_date), '/*.snappy.parquet'); 
+      EXPORT DATA OPTIONS (uri=(bucket_path), compression='Snappy', format='Parquet', overwrite=True) AS 
+      SELECT
+      *, (SELECT STRING_AGG(display_name, '/') FROM B.project.ancestors) organization_list 
+      FROM `${var.table_id}` as B 
+      WHERE usage_start_time >= start_date AND usage_start_time < end_date AND _PARTITIONTIME >= TIMESTAMP(EXTRACT(DATE FROM start_date)) 
+      LIMIT 9223372036854775807 ;
+    EOF
+  }
+}
 
 # Resource for Query2
-resource "google_bigquery_data_transfer_config" "query_config_2"{
-      display_name           = "corestack-monthly-query-${var.project_id}"
-      project                = var.project_id
-      location               = "${var.bucket_location}"
-      data_source_id         = "scheduled_query"
-      schedule               = "5 of month 00:00"
-      params = {
-        query = "DECLARE bucket_name STRING DEFAULT '${local.bucket_name}'; DECLARE current_date DATE DEFAULT CURRENT_DATE; DECLARE start_date TIMESTAMP DEFAULT TIMESTAMP(DATE_TRUNC(DATE_SUB(current_date, INTERVAL 1 MONTH), MONTH), 'US/Pacific'); DECLARE end_date TIMESTAMP DEFAULT TIMESTAMP(DATE_TRUNC(DATE_ADD(EXTRACT(DATE FROM start_date), INTERVAL 1 MONTH), MONTH), 'US/Pacific'); DECLARE bucket_path STRING DEFAULT CONCAT('gs://', bucket_name, '/', FORMAT_TIMESTAMP('%G-%m', start_date), '_backfill/*.csv'); EXPORT DATA OPTIONS (uri=(bucket_path), format='JSON', overwrite=True) AS SELECT *, (SELECT STRING_AGG(display_name, '/') FROM B.project.ancestors) organization_list FROM `${var.table_id}` as B WHERE usage_start_time >= start_date AND usage_start_time < end_date AND _PARTITIONTIME >= TIMESTAMP(EXTRACT(DATE FROM start_date)) ;"
+resource "google_bigquery_data_transfer_config" "query_config_2" {
+  display_name   = "corestack-monthly-query-${var.project_id}"
+  project        = var.project_id
+  location       = "${var.bucket_location}"
+  data_source_id = "scheduled_query"
+  schedule       = "5 of month 00:00"
 
-
-      }
-    }
+  params = {
+    query = <<-EOF
+      DECLARE bucket_name STRING DEFAULT '${local.bucket_name}';  
+      DECLARE current_date DATE DEFAULT CURRENT_DATE; 
+      --Backfill 
+      DECLARE start_date TIMESTAMP DEFAULT TIMESTAMP(DATE_TRUNC(DATE_SUB(current_date, INTERVAL 1 MONTH), MONTH), 'US/Pacific');  
+      DECLARE end_date TIMESTAMP DEFAULT TIMESTAMP(DATE_TRUNC(DATE_ADD(EXTRACT(DATE FROM start_date), INTERVAL 1 MONTH), MONTH), 'US/Pacific');  
+      DECLARE bucket_path STRING DEFAULT CONCAT('gs://', bucket_name, '/', FORMAT_TIMESTAMP('%G-%m', start_date), '_backfill/*.snappy.parquet'); 
+      EXPORT DATA OPTIONS (uri=(bucket_path), compression='Snappy', format='Parquet', overwrite=True) AS 
+      SELECT  
+      *, (SELECT STRING_AGG(display_name, '/') FROM B.project.ancestors) organization_list 
+      FROM `${var.table_id}` as B 
+      WHERE usage_start_time >= start_date AND usage_start_time < end_date AND _PARTITIONTIME >= TIMESTAMP(EXTRACT(DATE FROM start_date)) 
+      LIMIT 9223372036854775807 ;
+    EOF
+  }
+}
 
 # Resource for Query3
-resource "google_bigquery_data_transfer_config" "query_config_3"{
-      display_name           = "corestack-on-demand-1-${var.project_id}"
-      project                = var.project_id
-      location               = "${var.bucket_location}"
-      data_source_id         = "scheduled_query"
-      schedule               = "None"
-      params = {
-        query = "DECLARE bucket_name STRING DEFAULT '${local.bucket_name}'; DECLARE current_date DATE DEFAULT CURRENT_DATE; DECLARE start_date TIMESTAMP DEFAULT TIMESTAMP(DATE_TRUNC(DATE_SUB(current_date, INTERVAL 1 MONTH), MONTH), 'US/Pacific'); DECLARE end_date TIMESTAMP DEFAULT TIMESTAMP(DATE_TRUNC(DATE_ADD(EXTRACT(DATE FROM start_date), INTERVAL 1 MONTH), MONTH), 'US/Pacific'); DECLARE bucket_path STRING DEFAULT CONCAT('gs://', bucket_name, '/', FORMAT_TIMESTAMP('%G-%m', start_date), '_backfill/*.csv'); EXPORT DATA OPTIONS (uri=(bucket_path), format='JSON', overwrite=True) AS SELECT *, (SELECT STRING_AGG(display_name, '/') FROM B.project.ancestors) organization_list FROM `${var.table_id}` as B WHERE usage_start_time >= start_date AND usage_start_time < end_date AND _PARTITIONTIME >= TIMESTAMP(EXTRACT(DATE FROM start_date)) ;"
-      }
-    }
+resource "google_bigquery_data_transfer_config" "query_config_3" {
+  display_name   = "corestack-on-demand-1-${var.project_id}"
+  project        = var.project_id
+  location       = "${var.bucket_location}"
+  data_source_id = "scheduled_query"
+  schedule       = "None"
+
+  params = {
+    query = <<-EOF
+      DECLARE bucket_name STRING DEFAULT '${local.bucket_name}';  
+      DECLARE current_date DATE DEFAULT CURRENT_DATE; 
+      --Backfill 
+      DECLARE start_date TIMESTAMP DEFAULT TIMESTAMP(DATE_TRUNC(DATE_SUB(current_date, INTERVAL 1 MONTH), MONTH), 'US/Pacific');  
+      DECLARE end_date TIMESTAMP DEFAULT TIMESTAMP(DATE_TRUNC(DATE_ADD(EXTRACT(DATE FROM start_date), INTERVAL 1 MONTH), MONTH), 'US/Pacific');  
+      DECLARE bucket_path STRING DEFAULT CONCAT('gs://', bucket_name, '/', FORMAT_TIMESTAMP('%G-%m', start_date), '_backfill/*.snappy.parquet'); 
+      EXPORT DATA OPTIONS (uri=(bucket_path), compression='Snappy', format='Parquet', overwrite=True) AS 
+      SELECT  
+      *, (SELECT STRING_AGG(display_name, '/') FROM B.project.ancestors) organization_list 
+      FROM `${var.table_id}` as B 
+      WHERE usage_start_time >= start_date AND usage_start_time < end_date AND _PARTITIONTIME >= TIMESTAMP(EXTRACT(DATE FROM start_date)) 
+      LIMIT 9223372036854775807 ;
+    EOF
+  }
+}
 
 # Resource for Query4
-resource "google_bigquery_data_transfer_config" "query_config_4"{
-      display_name           = "corestack-on-demand-2-${var.project_id}"
-      project                = var.project_id
-      location               = "${var.bucket_location}"
-      data_source_id         = "scheduled_query"
-      schedule               = "None"
-      params = {
-        query = "DECLARE bucket_name STRING DEFAULT '${local.bucket_name}'; DECLARE current_date DATE DEFAULT CURRENT_DATE; DECLARE start_date TIMESTAMP DEFAULT TIMESTAMP(DATE_TRUNC(DATE_SUB(current_date, INTERVAL 2 MONTH), MONTH), 'US/Pacific'); DECLARE end_date TIMESTAMP DEFAULT TIMESTAMP(DATE_TRUNC(DATE_ADD(EXTRACT(DATE FROM start_date), INTERVAL 1 MONTH), MONTH), 'US/Pacific'); DECLARE bucket_path STRING DEFAULT CONCAT('gs://', bucket_name, '/', FORMAT_TIMESTAMP('%G-%m', start_date), '_backfill/*.csv'); EXPORT DATA OPTIONS (uri=(bucket_path), format='JSON', overwrite=True) AS SELECT *, (SELECT STRING_AGG(display_name, '/') FROM B.project.ancestors) organization_list FROM `${var.table_id}` as B WHERE usage_start_time >= start_date AND usage_start_time < end_date AND _PARTITIONTIME >= TIMESTAMP(EXTRACT(DATE FROM start_date)) ;"
-      }
-    }
+resource "google_bigquery_data_transfer_config" "query_config_4" {
+  display_name   = "corestack-on-demand-2-${var.project_id}"
+  project        = var.project_id
+  location       = "${var.bucket_location}"
+  data_source_id = "scheduled_query"
+  schedule       = "None"
+
+  params = {
+    query = <<-EOF
+      DECLARE bucket_name STRING DEFAULT '${local.bucket_name}';  
+      DECLARE current_date DATE DEFAULT CURRENT_DATE; 
+      --Backfill 
+      DECLARE start_date TIMESTAMP DEFAULT TIMESTAMP(DATE_TRUNC(DATE_SUB(current_date, INTERVAL 2 MONTH), MONTH), 'US/Pacific');  
+      DECLARE end_date TIMESTAMP DEFAULT TIMESTAMP(DATE_TRUNC(DATE_ADD(EXTRACT(DATE FROM start_date), INTERVAL 1 MONTH), MONTH), 'US/Pacific');  
+      DECLARE bucket_path STRING DEFAULT CONCAT('gs://', bucket_name, '/', FORMAT_TIMESTAMP('%G-%m', start_date), '_backfill/*.snappy.parquet'); 
+      EXPORT DATA OPTIONS (uri=(bucket_path), compression='Snappy', format='Parquet', overwrite=True) AS 
+      SELECT  
+      *, (SELECT STRING_AGG(display_name, '/') FROM B.project.ancestors) organization_list 
+      FROM `${var.table_id}` as B 
+      WHERE usage_start_time >= start_date AND usage_start_time < end_date AND _PARTITIONTIME >= TIMESTAMP(EXTRACT(DATE FROM start_date)) 
+      LIMIT 9223372036854775807 ;
+    EOF
+  }
+}
 
 # Resource for Query5
-resource "google_bigquery_data_transfer_config" "query_config_5"{
-      display_name           = "corestack-on-demand-3-${var.project_id}"
-      project                = var.project_id
-      location               = "${var.bucket_location}"
-      data_source_id         = "scheduled_query"
-      schedule               = "None"
-      params = {
-        query = "DECLARE bucket_name STRING DEFAULT '${local.bucket_name}'; DECLARE current_date DATE DEFAULT CURRENT_DATE; DECLARE start_date TIMESTAMP DEFAULT TIMESTAMP(DATE_TRUNC(DATE_SUB(current_date, INTERVAL 3 MONTH), MONTH), 'US/Pacific'); DECLARE end_date TIMESTAMP DEFAULT TIMESTAMP(DATE_TRUNC(DATE_ADD(EXTRACT(DATE FROM start_date), INTERVAL 1 MONTH), MONTH), 'US/Pacific'); DECLARE bucket_path STRING DEFAULT CONCAT('gs://', bucket_name, '/', FORMAT_TIMESTAMP('%G-%m', start_date), '_backfill/*.csv'); EXPORT DATA OPTIONS (uri=(bucket_path), format='JSON', overwrite=True) AS SELECT *, (SELECT STRING_AGG(display_name, '/') FROM B.project.ancestors) organization_list FROM `${var.table_id}` as B WHERE usage_start_time >= start_date AND usage_start_time < end_date AND _PARTITIONTIME >= TIMESTAMP(EXTRACT(DATE FROM start_date)) ;"
-      }     
-        
+resource "google_bigquery_data_transfer_config" "query_config_5" {
+  display_name   = "corestack-on-demand-3-${var.project_id}"
+  project        = var.project_id
+  location       = "${var.bucket_location}"
+  data_source_id = "scheduled_query"
+  schedule       = "None"
+
+  params = {
+    query = <<-EOF
+      DECLARE bucket_name STRING DEFAULT '${local.bucket_name}';  
+      DECLARE current_date DATE DEFAULT CURRENT_DATE; 
+      --Backfill 
+      DECLARE start_date TIMESTAMP DEFAULT TIMESTAMP(DATE_TRUNC(DATE_SUB(current_date, INTERVAL 3 MONTH), MONTH), 'US/Pacific');  
+      DECLARE end_date TIMESTAMP DEFAULT TIMESTAMP(DATE_TRUNC(DATE_ADD(EXTRACT(DATE FROM start_date), INTERVAL 1 MONTH), MONTH), 'US/Pacific');  
+      DECLARE bucket_path STRING DEFAULT CONCAT('gs://', bucket_name, '/', FORMAT_TIMESTAMP('%G-%m', start_date), '_backfill/*.snappy.parquet'); 
+      EXPORT DATA OPTIONS (uri=(bucket_path), compression='Snappy', format='Parquet', overwrite=True) AS 
+      SELECT  
+      *, (SELECT STRING_AGG(display_name, '/') FROM B.project.ancestors) organization_list 
+      FROM `${var.table_id}` as B 
+      WHERE usage_start_time >= start_date AND usage_start_time < end_date AND _PARTITIONTIME >= TIMESTAMP(EXTRACT(DATE FROM start_date)) 
+      LIMIT 9223372036854775807 ;
+    EOF
+  }
 }
+
 
 resource "google_project_service" "api_proj_enabler" {
     project = var.project_id
